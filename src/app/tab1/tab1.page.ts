f5a0f31875aae40dff592db2d16a507e83aa2c90
Richardtex22 <jraguilars@icloud.com>
2019-02-11 07:33:41 -0600

    Typescript error

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -25,30 +25,14 @@ mostrarBoton: boolean = false;
 constructor(public navCtrl: NavController, @Inject(DOCUMENT) document) {
 	this.user = JSON.parse(localStorage.getItem('infoToken'));
 }
-/*const controls = this.cardOrders.map(c => new FormControl(false));
-    controls[0].setValue(true);
-
-    this.form = this.formBuilder.group({
-      cardOrders: new FormArray(controls)
-    });
-}
-
-cardsApi() {
-    const selectedOrderIds = this.form.value.cardOrders
-      .map((v, i) => v ? this.cardOrders[i].id : null)
-      .filter(v => v !== null);
-
-    console.log(selectedOrderIds);
-  }*/
 
     ngClick() {
-        let tarjetaName = document.getElementById('tarjeta').value;
+        let tarjetaName = (<HTMLInputElement><any>document.getElementById('tarjeta')).value;
         let tarjetaNameOf = null;
         var espacio = ' ';
         var arrayDeCadenas = tarjetaName.split(espacio);
         var cadTamaño = arrayDeCadenas.length - 1;
         let tipoT = arrayDeCadenas[cadTamaño];
-        console.log(tipoT);
         localStorage.setItem('type', tipoT);
         if(tipoT === 'TDD') {
         	tarjetaNameOf = 'Tarjeta Azul';
@@ -83,11 +67,32 @@ cardsApi() {
 	  )
 
 	 let infoTarjetas = JSON.parse(localStorage.getItem('type_cards'));
-	 // console.log(infoTarjetas);
 	 this.cardCero = infoTarjetas.type_cards;
 	 this.mostrarCuentas = true;
   	}
 
+misCuentas() {
+  	var url = 'https://mighty-refuge-81707.herokuapp.com/api/accounts';
+	var token = localStorage.getItem('token');
+  	var myHeaders = new Headers();
+	myHeaders.append("Content-Type", "application/json");
+	myHeaders.append("X-access-token", token);
+
+  	fetch(url, {
+	    method: 'GET',
+	    
+	   headers: myHeaders
+	})
+	  .then(response => response.json())
+	  .then(json => {
+	  	localStorage.setItem('type_cards', JSON.stringify(json.response))
+	  	}, 
+	  )
+
+	 // let infoTarjetas = JSON.parse(localStorage.getItem('type_cards'));
+	 // console.log(infoTarjetas);
+	 
+  	}
 
 
  newAccount() {
